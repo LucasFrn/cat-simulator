c9b9e0dc91229e0ca28dab28061f98eb3bd0e020
Wagner Lucas <wlucas.1.5@gmail.com>
2023-09-24 14:24:39 -0300

    Criado GameManager
    Começando a armazenar status do player

---
+++ SimuGato/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int petiscos, fome, energia, higiene, felicidade, social;
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+}
--- SimuGato/Assets/Scripts/MainSceneScripts/Player.cs
+++ SimuGato/Assets/Scripts/MainSceneScripts/Player.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+
+    int petiscos, fome, energia, higiene, felicidade, social;
     // Start is called before the first frame update
     void Start()
     {
-        
+        petiscos = GameManager.Instance.petiscos;
+        fome = GameManager.Instance.fome;
+        energia = GameManager.Instance.energia;
+        higiene = GameManager.Instance.higiene;
+        felicidade = GameManager.Instance.felicidade;
+        social = GameManager.Instance.social;
     }
 
     // Update is called once per frame
-    void Update()
+    void TransferStatus()
     {
-        
+        GameManager.Instance.petiscos = petiscos;
+        GameManager.Instance.fome = fome;
+        GameManager.Instance.energia = energia;
+        GameManager.Instance.higiene = higiene;
+        GameManager.Instance.felicidade = felicidade;
+        GameManager.Instance.social = social;
     }
+    
 }
