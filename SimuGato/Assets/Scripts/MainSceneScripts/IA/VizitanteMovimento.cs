8eb0a4b6ab45c9accb71a8c337fec824ebacdb63
LucasFrn <wlucas.1.5@gmail.com>
2023-10-29 18:14:55 -0300

    IA do visitante aprimorada

--- SimuGato/Assets/Scripts/MainSceneScripts/IA/VizitanteMovimento.cs
+++ SimuGato/Assets/Scripts/MainSceneScripts/IA/VizitanteMovimento.cs
@@ -5,78 +5,94 @@ using UnityEngine.AI;
 public class VizitanteMovimento : MonoBehaviour
 {
     private NavMeshAgent m_Agent;
-    //private LuzManager luz;
-    //public GameObject manager;
+    private LuzManager luz;
+    public GameObject manager;
     public List<GameObject> bancos;
     public GameObject saida;
 
     private int index = 0;
     private bool interagindo = false;
     private bool temporizador = false;
+    private bool gatoArea = false;
     private float tempoRestante = 20;
     private void Awake()
     {
         m_Agent = GetComponent<NavMeshAgent>();
-        //luz = manager.GetComponent<LuzManager>();
+        luz = manager.GetComponent<LuzManager>();
 
     }
     private void FixedUpdate()
     {
-        //if (luz.HoraDoDia<8 || luz.HoraDoDia>17)
-        //{
-        //m_Agent.destination = saida.transform.position;
-        // }
-        // else
-        //{
-        //if (temporizador)
-        //{
-        // if (tempoRestante > 0)
-        // {
-        //tempoRestante -= Time.fixedDeltaTime;
-        //}
-        //else
-        // {
-        // temporizador = false;
-        // interagindo = false;
-        // tempoRestante = 20;
-        //}
-        //}
-        if (!interagindo)
+        if (luz.HoraDoDia < 8 || luz.HoraDoDia > 17)
         {
-            m_Agent.destination = bancos[index].transform.position;
+            
+            m_Agent.SetDestination(saida.transform.position);
         }
-        float distancia = Vector3.Distance(transform.position, m_Agent.destination);
-        if (distancia < 1)
+        else
         {
-            if (index <= bancos.Count - 1)
+            if (gatoArea)
             {
-                index++;
+                m_Agent.SetDestination(transform.position);                
             }
             else
             {
-                index = 0;
+                if (temporizador)
+                {
+                    if (tempoRestante > 0)
+                    {
+                        Debug.Log(tempoRestante);
+                        tempoRestante -= Time.fixedDeltaTime;
+                    }
+                    else
+                    {
+                        Debug.Log("Acabou");
+                        temporizador = false;
+                        interagindo = false;
+                        tempoRestante = 20;
+                        if (index < bancos.Count - 1)
+                        {
+                            index++;
+                        }
+                        else
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    if (!interagindo)
+                    {
+                        m_Agent.SetDestination(bancos[index].transform.position);
+                    }
+                    float distancia = Vector3.Distance(transform.position, m_Agent.destination);
+                    if (distancia < 3)
+                    {
+                        
+                        temporizador = true;
+                        interagindo = true;
+
+                    }
+                }
             }
-            // }
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Banco")
+
+        if (collision.gameObject.tag == "Player")
         {
-            temporizador = true;
-            interagindo = true;
-        }
-        else if (collision.gameObject.tag == "Player")
-        {
-            interagindo = true;
+            Debug.Log("Gato");
+            gatoArea = true;
         }
     }
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if (!temporizador) { interagindo = false; }
+            Debug.Log("Saiu gato");
+            gatoArea = false;
         }
     }
 
