f8ce8f07c8c1f43981056db22982374be09aab7d
gs144 <otaviobiavateoliveira@hotmail.com>
2023-12-07 20:06:48 -0300

    main menu start

--- SimuGato/Assets/Scripts/MainSceneScripts/UIController.cs
+++ SimuGato/Assets/Scripts/MainSceneScripts/UIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mail;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
@@ -69,5 +70,11 @@ public class UIController : MonoBehaviour
     }
     public void Sair(){
         Application.Quit();
+    }
+
+    public void Jogar() 
+    {
+        SceneManager.LoadScene("TestNavMesh");
+
     }
 }
